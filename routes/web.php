6526ce99157c37740bbb21e1b58d978ead6f223a
Robson <robsonkumagai@gmail.com>
2017-11-02 11:06:19 -0200

    Correções nas rotas web e api

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/buybox', 'HomeController@graficos')->name('buybox');
 Route::get('/relatorio', 'HomeController@relatorio')->name('relatorio');
+Route::get('/blog', 'HomeController@blog')->name('blog');
 Route::resource('portal', 'PortalController');
 
 Route::get('portal',
     ['as' => 'portal', 'uses' => 'PortalController@index']);
 
+//Rota que o form de envio de email do home utiliza
 Route::post('portal',
-    ['as' => 'portal_store', 'uses' => 'PortalController@store']);
+    ['as' => 'cadastro_email', 'uses' => 'PortalController@cadastroEmail']);
+
+//Rota que o form de envio de email de suporte utiliza
+Route::post('contato',
+    ['as' => 'contato_email', 'uses' => 'HomeController@contatoEmail']);
+
+//Route::post('/contatoemail', 'HomeController@contatoEmail');
